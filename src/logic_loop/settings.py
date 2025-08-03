e06f7ec8356931a82912b7a9b53da8cdd198df27
Ridwan Rahman <ridwanrahman07@gmail.com>
2025-08-03 20:37:42 +1000

    Added html page, gunicorn and updated readme

--- src/logic_loop/settings.py
+++ src/logic_loop/settings.py
@@ -21,6 +21,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 APPS_DIR = BASE_DIR / 'apps'
+TEMPLATES_DIR = BASE_DIR / 'frontend' / 'templates'
+STATIC_DIR = BASE_DIR / 'frontend' / 'static'
 # Add the apps directory to Python path
 sys.path.insert(0, str(BASE_DIR / 'apps'))
 
--- src/logic_loop/views.py
+++ src/logic_loop/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def health_check(request):
@@ -6,3 +7,9 @@ def health_check(request):
     Health check endpoint to verify API is running
     """
     return HttpResponse("Application is running", content_type="text/plain")
+
+def show_homepage(request):
+    """
+    Show the homepage
+    """
+    return render(request, 'pages/hello-world.html', {})
