88fb5cfc5bbd8ce5212ec533c88e8fc720f945c0
Ridwan Rahman <ridwanrahman07@gmail.com>
2025-08-24 22:05:38 +1000

    Removed unused 3rd party lib

--- src/logic_loop/settings.py
+++ src/logic_loop/settings.py
@@ -47,9 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 3rd party libs
-    'django_seed',
-
     'categories',
     'common',
     'discussion',
