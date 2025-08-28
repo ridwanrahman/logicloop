7edc04b8166d9c9de62854ac2f4d8590e065e728
Ridwan Rahman <ridwanrahman07@gmail.com>
2025-08-28 23:20:20 +1000

    trying out users html

--- src/logic_loop/settings.py
+++ src/logic_loop/settings.py
@@ -128,7 +128,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+APPEND_SLASH = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
